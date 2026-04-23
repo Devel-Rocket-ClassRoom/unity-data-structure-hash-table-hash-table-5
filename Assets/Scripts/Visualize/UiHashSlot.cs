fc0bd8f8091c609b1aee5e66d6f12c4054ba9395
smflsmfqh <skyee46@gmail.com>
2026-04-23 23:03:59 +0900

    Test : Create Slots depending on hash table mode

---
+++ Assets/Scripts/Visualize/UiHashSlot.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class UiHashSlot : MonoBehaviour
+{    
+    public TextMeshProUGUI indexText;
+    public TextMeshProUGUI kvText;
+    public Image background;
+}
