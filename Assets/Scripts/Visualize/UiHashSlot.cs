0a46572189ac0857540fc75fc4e383a8b8d1a238
kimjunyoung <suk558165@gmail.com>
2026-04-24 13:39:39 +0900

    ..

--- Assets/Scripts/Visualize/UiHashSlot.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
-public class UiHashSlot : MonoBehaviour
-{    
-    public TextMeshProUGUI indexText;
-    public TextMeshProUGUI kvText;
-    public Image background;
-}
